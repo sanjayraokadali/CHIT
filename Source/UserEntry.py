44fc77dbb811c30d5b966e4139569082e48496d4
Sanjay Rao Kadali <62662617+sanjayraokadali@users.noreply.github.com>
2022-02-06 21:58:16 +0530

    Cleaning Up

--- Source/UserEntry.py
+++ Source/UserEntry.py
@@ -23,10 +23,6 @@ data = Data(file_path=user_data[-1],
             data=user_data
             )
 
-data.write_to_csv()
-
-struct_data = StructData()
-
 sqldb = SqlDB("localhost", "sanjayraokadali", "23March_an", "UserDB")
 
 insertion = sqldb.insert_user(obj = sqldb, data = user, file_loc = user_data[-1])
@@ -34,3 +30,6 @@ insertion = sqldb.insert_user(obj = sqldb, data = user, file_loc = user_data[-1]
 if insertion:
 
     print("User inserted to Database")
+else:
+
+    print("User data not inserted to the Database")
